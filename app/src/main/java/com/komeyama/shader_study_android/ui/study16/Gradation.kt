d9632ab5d1d594206ac7bfcad29744e68245d6a6
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2023-05-05 23:59:41 +0900

    remove unnecessary

--- app/src/main/java/com/komeyama/shader_study_android/ui/study16/Gradation.kt
+++ app/src/main/java/com/komeyama/shader_study_android/ui/study16/Gradation.kt
@@ -21,8 +21,8 @@ class Gradation(
 ) : ShaderBase() {
 
     sealed class GradationShaderType {
-        data class Vertical(val ids: Int = 0): GradationShaderType()
-        data class Circle(val ids: Int = 1): GradationShaderType()
+        data class Vertical(val ids: Int = 0) : GradationShaderType()
+        data class Circle(val ids: Int = 1) : GradationShaderType()
     }
 
     var color = initColor
@@ -34,7 +34,8 @@ class Gradation(
 
     init {
         val vertexShader = ShaderUtils.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode())
-        val fragmentShader = ShaderUtils.loadShader(GLES20.GL_FRAGMENT_SHADER, LinearInterpolationShaderCodes.CIRCLE)
+        val fragmentShader =
+            ShaderUtils.loadShader(GLES20.GL_FRAGMENT_SHADER, LinearInterpolationShaderCodes.CIRCLE)
         circleProgram = ShaderUtils.createProgram(vertexShader, fragmentShader)
     }
 
@@ -52,19 +53,15 @@ class Gradation(
     }
 
     fun setProgram(type: GradationShaderType) {
-        val program = when(type) {
+        val program = when (type) {
             is GradationShaderType.Vertical -> verticalProgram
-            is GradationShaderType.Circle ->circleProgram
+            is GradationShaderType.Circle -> circleProgram
         }
         GLES20.glUseProgram(program)
     }
 
     override fun handleFragmentParameter() {
         GLES20.glGetUniformLocation(program, "vResolution").also { resolutionHandle ->
-            GLES20.glUniform2fv(resolutionHandle, 1, resolution, 0)
-        }
-
-        GLES20.glGetUniformLocation(circleProgram, "vResolution").also { resolutionHandle ->
             GLES20.glUniform2fv(resolutionHandle, 1, resolution, 0)
         }
     }
